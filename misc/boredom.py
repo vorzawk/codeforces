ae7a04848bb4629375d42dfd91bb532c2c54a43e
vorzawk <21965736+vorzawk@users.noreply.github.com>
2020-02-05 22:21:36 +0530

    practice and new contests

--- misc/boredom.py
+++ misc/boredom.py
@@ -1,17 +1,18 @@
 N = int(input())
 a = list(map(int, input().split()))
-freq = {}
+n = max(a) + 1
+freq = [0] * n
 for e in a:
-    if e not in freq:
-        freq[e] = 0
     freq[e] += 1
-a = list(set(a))
-a.sort()
-n = len(a)
-res = [None] * (n+1)
+res = [0] * n
 res[0] = 0
-res[1] = freq[a[0]] * a[0]
-for i in range(2, n+1):
-    res[i] = max(res[i-2] + a[i-1] * freq[a[i-1]], res[i-1])
-print(res[n])
+l = min(a)
+res[l] = freq[l] * l
+if max(a) == 1:
+    print(res[1])
+else:
+    for i in range(2, n):
+#        print(res, freq)
+        res[i] = max(res[i-2] + i * freq[i], res[i-1])
+    print(res[-1])
 
